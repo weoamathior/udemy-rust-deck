41a5c7b4235f681607a9c58d2cde3a0035fa616a
weoamathior <weoamathior@gmail.com>
2024-08-26 16:46:10 -0700

    end of lecture 11

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 #[derive(Debug)]
 struct Deck {
-    cards: Vec<String>
+    cards: Vec<String>,
+}
+
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
+        let values = ["Ace", "Two", "Three"];
+
+        let mut cards = vec![];
+
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
+        }
+
+        let deck = Deck { cards };
+        return deck;
+    }
 }
 fn main() {
-    let suits = ["Hearts", "Spades", "Diamonds", "Clubs"];
-    let values = ["Ace","Two","Three"];
-
-    let mut cards =  vec![];
-
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
-        }
-    }
-
-    let deck = Deck {cards};
+    let deck = Deck::new();
     println!("Here's your deck: {:#?}", deck);
-
 }
