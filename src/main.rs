a1300d59fece8401c820c28809db3d43567bb00c
weoamathior <weoamathior@gmail.com>
2024-09-01 08:00:11 -0700

    end of section 2

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,24 @@ impl Deck {
         self.cards.shuffle(&mut rng);
 
     }
+
+    fn deal(&mut self, num_cards: usize )-> Vec<String> {
+
+        // implicit return
+        self.cards.split_off(
+            self.cards.len() - num_cards
+        )
+        
+    }
 }
 fn main() {
     let mut deck = Deck::new();
-    deck.shuffle();
+    // deck.shuffle();
 
+    let mut dealed: Vec<String> = deck.deal(7);
+
+    // println!("Here's your deck: {:#?}", deck);
+    println!("Here's your deal : {:#?}", dealed);
     println!("Here's your deck: {:#?}", deck);
+
 }
